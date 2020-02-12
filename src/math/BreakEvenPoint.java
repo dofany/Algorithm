90c73d8adae2620ed4a99b262f5782a191e7a1f7
dofany <kimdohwan17@gmail.com>
2020-02-13 04:24:30 +0900

    백준 알고리즘

--- src/math/BreakEvenPoint.java
+++ src/math/BreakEvenPoint.java
@@ -1,10 +1,19 @@
 package math;
 
+import java.util.*;
+
 public class BreakEvenPoint {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long c = sc.nextLong();
+		
+		if((c-b)>0) {
+			System.out.println((a/(c-b))+1);
+		}else {
+			System.out.println("-1");
+		}
 	}
-
 }
