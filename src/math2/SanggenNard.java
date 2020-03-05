9a67abd909cb9c70000d9b941ae9e80e35a4b558
dofany <kimdohwan17@gmail.com>
2020-03-05 17:34:38 +0900

    백준 알고리즘

---
+++ src/math2/SanggenNard.java
@@ -0,0 +1,30 @@
+package math2;
+
+import java.util.*;
+
+public class SanggenNard {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int res = 0;
+		int min = 0;
+		int input;
+		
+		min = sc.nextInt();
+		for(int i = 0; i<2; i++) {
+			input = sc.nextInt();
+			if(min>input)
+				min = input;
+		}
+		res += min;
+		
+		min = 0;
+		min = sc.nextInt();
+		for(int i = 0; i<1; i++) {
+			input = sc.nextInt();
+			if(min>input)
+				min = input;
+		}
+		res+=(min-50);
+		System.out.println(res);
+	}
+}
