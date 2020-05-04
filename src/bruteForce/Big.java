888ea3269f67c242c0bddbc74b8f629cddb6b804
dofany <kimdohwan17@gmail.com>
2020-05-04 22:20:34 +0900

    백준 알고리즘

--- src/bruteForce/Big.java
+++ src/bruteForce/Big.java
@@ -7,16 +7,27 @@ public class Big {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
+		int[][] a = new int[t][2];
+		int[] b = new int[t];
+		
 		for(int i = 0; i<t; i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			int[] size = {x,y};
-			int rank[] = null;
-			rank[i] = 1;
-			
-			for(int j = 0; j<size.length; j++) {
-				
+			a[i][0] = sc.nextInt();
+			a[i][1] = sc.nextInt();
+		}
+		for(int i =0; i<t; i++) {
+			for(int j = 0; j<t; j++) {
+				if(i!=j && a[i][0]<a[j][0] &&a[i][1]<a[j][1]) {
+					b[i]++;
+				}
 			}
+		}
+		for(int i =0; i<t; i++) {
+			System.out.print(b[i]+1);
+			if(i!=t-1) {
+				System.out.print(" ");
+			}
+			else
+				System.out.println();
 		}
 	}
 }
