a203e27d42df248c6883fd35016e790000544d70
dofany <kimdohwan17@gmail.com>
2021-03-10 18:54:01 +0900

    백준 알고리즘

--- src/listSort/NumSort2.java
+++ src/listSort/NumSort2.java
@@ -6,15 +6,20 @@ import java.util.*;
 public class NumSort2 {
 
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int array = Integer.parseInt(br.readLine());
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		for(int i = 0; i<array; i++) {
-			arr.add(Integer.parseInt(br.readLine()));
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int n = sc.nextInt();
+
+		ArrayList<Integer> arr = new ArrayList<>();
+
+		for(int i = 0; i< n; i++){
+			arr.add(sc.nextInt());
 		}
 		Collections.sort(arr);
-		for(int i = 0; i<arr.size(); i++) {
-			System.out.println(arr.get(i));
+
+		for(int value : arr){
+			sb.append(value).append('\n');
 		}
+		System.out.println(sb);
 	}
 }
